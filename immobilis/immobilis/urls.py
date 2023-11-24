c8868e523a39c4682838a741212aac2af607af37
Tiago Sales <tiagosales.ti@gmail.com>
2023-11-23 22:55:03 -0300

    redirecionar pagina inicial para busca

--- immobilis/immobilis/urls.py
+++ immobilis/immobilis/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
+#urlpatterns = [
+#    path('admin/', admin.site.urls),
+#    path('', include('web.urls')),
+#]
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('web.urls')),
-
+    path('', lambda req: redirect('/busca/'))
 ]
